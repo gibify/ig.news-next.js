a535982ddf72dc2029f4da75dfb1bf0e0cc1bfcc
gibifyofficial <evandrogibicoski@outlook.com>
2021-04-18 12:04:21 -0300

    add favicon and setting Oauth github

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,15 +1,46 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { query as q } from 'faunadb';
 import { stripe } from '../../services/stripe';
+import { fauna } from "../../services/faunadb";
+
+type User = {
+    ref: {
+        id: string;
+    }
+}
 
 export default async(req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === "POST") {
         const session = await getSession({ req });
 
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email),
+                )
+            )
+        )
+
         const stripeCustomer = await stripe.customers.create({
             email: session.user.email,
             // metadata
         });
+
+
+        await fauna.query(
+            q.Update(
+                q.Ref(q.Collection('users'), user.ref.id),
+                { 
+                   data: { 
+                       stripe_customer_id: stripeCustomer.id,
+                   } 
+                }
+            )
+        )
+
+
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
             customer: stripeCustomer.id,
