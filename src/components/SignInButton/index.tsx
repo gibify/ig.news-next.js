12ee97b81fa99ef80dee4a88ae7d6eab931ca69a
gibify <gibifyofficial@gmail.com>
2022-06-25 13:54:36 -0300

    feat: ignite

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,26 +7,24 @@ import styles from './styles.module.scss';
 export function SignInButton() {
     const [session] = useSession();
 
-    console.log(session);
-
     return session ? (
-        <button 
-        type="button" 
-        className={styles.signInButton}
-        onClick={() => signOut()}
+        <button
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signOut()}
 
         >
-            <FaGithub color="#04d361"/>
+            <FaGithub color="#04d361" />
             {session.user.name}
-            <FiX  color="#737380" className={styles.closeIcon}/>
+            <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button 
-        type="button" 
-        className={styles.signInButton}
-        onClick={() => signIn('github')}
+        <button
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
-            <FaGithub color="eba417"/>
+            <FaGithub color="eba417" />
             Sigin with Github
         </button>
     );
